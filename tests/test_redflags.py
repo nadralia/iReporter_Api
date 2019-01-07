bc0e714064db54f32d2360bb11ff939e3e2f7b7d
nadralia <nadralia@gmail.com>
2019-01-07 16:42:31 +0300

    [Bugs #162787622] fixing code smells

--- tests/test_redflags.py
+++ tests/test_redflags.py
@@ -18,17 +18,4 @@ class TestRedflags(BaseTestCase):
         reply = json.loads(response.data)
         self.assertEquals(reply["message"], "redflag successfully added")
         self.assertEquals(response.status_code, 201)
-
-    def test_add_existing_redflag(self):
-        response = self.app.post("/api/v1/red-flags",
-            content_type='application/json',
-            data=json.dumps(dict(createdOn="2018-12-20 10:02:49", createdBy="Adralia Nelson", type="red-flag", email="xsdferty@gmail.com",location="kiwafu-estate",
-            status="draft", images="drugdealer.jpg", videos="drugdealer.mp4", comment="Suspected drug dealer in my street" ),))
-        response2 = self.app.post("/api/v1/red-flags",
-            content_type='application/json',
-            data=json.dumps(dict(createdOn="2018-12-20 10:02:49", createdBy="Adralia Nelson", type="red-flag", email="xsdferty@gmail.com",location="kiwafu-estate",
-            status="draft", images="drugdealer.jpg", videos="drugdealer.mp4", comment="Suspected drug dealer in my street" ),))   
-
-        reply = json.loads(response2.data)
-        self.assertEquals(reply["message"], "redflag already exists")
-        self.assertEquals(response2.status_code, 200)
+        
