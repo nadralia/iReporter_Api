97eb2390c180bf4ef6df0697c507c3151de52cd4
nadralia <nadralia@gmail.com>
2019-01-11 15:24:15 +0300

    [Delivers #163116506] redflag tests scripts

--- tests/test_users.py
+++ tests/test_users.py
@@ -2,7 +2,16 @@ from api.models.user import User
 from tests.base_test import BaseTestCase
 from flask import json
 
-user_obj = User()
 
 class TestRedflags(BaseTestCase):
-    pass
+    def setUp(self):
+        kwargs = {
+            "user_id": 1,
+            "full_name": "sarah",
+            "email": "nadralia@gmail.com",
+            "gender": "male",
+            "username": "nadralia",
+            "password": "1234567#ad",
+            "role": "admin"
+        }
+        self.user = User(**kwargs)
