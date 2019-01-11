27084ea380edc0b3139cda8244e271d35598d1de
nadralia <nadralia@gmail.com>
2019-01-11 15:16:23 +0300

    [Delivers #163116506] redflag tests scripts

--- tests/test_redflags.py
+++ tests/test_redflags.py
@@ -16,6 +16,50 @@ class TestRedflags(BaseTestCase):
             status="draft", images="poweroutage.jpg", videos="poweroutage.mp4", comment="Power outage for 2 over weeks" ),))
 
         reply = json.loads(response.data)
-        self.assertEquals(reply["message"], "redflag successfully added")
-        self.assertEquals(response.status_code, 201)
-        
+        self.assertEqual(reply["message"], "redflag successfully added")
+        self.assertEqual(response.status_code, 201)
+
+    def test_adding_redflag_with_missing_key(self):
+        response = self.app.post("/api/v1/red-flags",
+            content_type='application/json',
+            data=json.dumps(dict(type="red-flag", comment="Power outage for 2 over weeks"),))
+
+        reply = json.loads(response.data)
+        self.assertEqual(reply["message"], "a 'key(s)' is missing in your request body")
+        self.assertEqual(response.status_code, 400)   
+
+    def test_fetching_redflags(self):
+        response = self.app.post("/api/v1/red-flags",
+            content_type='application/json',
+            data=json.dumps(dict(createdOn="2018-12-20 10:02:49", createdBy="Adralia Nelson", type="red-flag", email="xsdferty@gmail.com",location="kiwafu-estate",
+            status="draft", images="poweroutage.jpg", videos="poweroutage.mp4", comment="Power outage for 2 over weeks"),))
+
+        reply = json.loads(response.data.decode())
+        response2 = self.app.get("/api/v1/red-flags",
+        content_type='application/json')
+        reply2 = json.loads(response2.data.decode())
+        self.assertEqual(response2.status_code, 200)     
+
+    def test_fetching_single_redflag(self):
+        response = self.app.post("/api/v1/red-flags",
+            content_type='application/json',
+            data=json.dumps(dict(createdOn="2018-12-20 10:02:49", createdBy="Adralia Nelson", type="red-flag", email="xsdferty@gmail.com",location="kiwafu-estate",
+            status="draft", images="poweroutage.jpg", videos="poweroutage.mp4", comment="Power outage for 2 over weeks"),))
+
+        reply = json.loads(response.data.decode())
+        response2 = self.app.get("/api/v1/red-flags/2",
+        content_type='application/json')
+        reply2 = json.loads(response2.data.decode())
+        self.assertEqual(response2.status_code, 200)
+
+    def test_fetching_single_redflag_with_impromper_id(self):
+        response = self.app.post("/api/v1/red-flags",
+            content_type='application/json',
+            data=json.dumps(dict(createdOn="2018-12-20 10:02:49", createdBy="Adralia Nelson", type="red-flag", email="xsdferty@gmail.com",location="kiwafu-estate",
+            status="draft", images="poweroutage.jpg", videos="poweroutage.mp4", comment="Power outage for 2 over weeks"),))
+
+        reply = json.loads(response.data.decode())
+        response2 = self.app.get("/api/v1/red-flags/r",
+        content_type='application/json')
+        reply2 = json.loads(response2.data.decode())
+        self.assertEqual(response2.status_code, 400)  
---
+++ tests/test_users.py
@@ -0,0 +1,8 @@
+from api.models.user import User
+from tests.base_test import BaseTestCase
+from flask import json
+
+user_obj = User()
+
+class TestRedflags(BaseTestCase):
+    pass
